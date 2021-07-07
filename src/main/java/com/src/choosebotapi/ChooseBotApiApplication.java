bf743ce90e2b47b4e33d8f8623465410e26dbe0e
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2021-07-07 15:14:10 +0300

    initial commit

---
+++ src/main/java/com/src/choosebotapi/ChooseBotApiApplication.java
@@ -0,0 +1,13 @@
+package com.src.choosebotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseBotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChooseBotApiApplication.class, args);
+    }
+
+}
