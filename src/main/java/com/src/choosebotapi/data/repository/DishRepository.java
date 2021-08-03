df08716e020a70a008dde35af29b311145a6c17b
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2021-08-03 22:41:43 +0300

    ultra minor

--- src/main/java/com/src/choosebotapi/data/repository/DishRepository.java
+++ src/main/java/com/src/choosebotapi/data/repository/DishRepository.java
@@ -1,15 +1,21 @@
 package com.src.choosebotapi.data.repository;
 
 import com.src.choosebotapi.data.model.Dish;
-import com.src.choosebotapi.data.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestController
 public interface DishRepository extends JpaRepository<Dish, Long> {
     Optional<Dish> getDishByNameAndRestaurant_NameAndRestaurant_Address(@NotEmpty @NotNull String name, @NotNull String restaurant_name, String restaurant_address);
+
+    @Query(value = "select * " +
+            "from dish" +
+            "", nativeQuery = true)
+    List<Dish> findTop10ByRating();
 }
