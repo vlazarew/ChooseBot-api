e05d728aaeeb2753cc819b4d1bd2f664c38e142b
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2021-10-30 12:47:56 +0300

    added user blocked status

--- src/main/java/com/src/choosebotapi/data/repository/telegram/TelegramUserRepository.java
+++ src/main/java/com/src/choosebotapi/data/repository/telegram/TelegramUserRepository.java
@@ -4,6 +4,11 @@ import com.src.choosebotapi.data.model.telegram.TelegramUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+import java.util.Optional;
+
 @RepositoryRestController
 public interface TelegramUserRepository extends CrudRepository<TelegramUser, Long> {
+
+    Optional<TelegramUser> findById(Long id);
+
 }
